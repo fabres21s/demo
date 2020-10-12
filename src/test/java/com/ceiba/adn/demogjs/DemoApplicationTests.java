9ad8cc3be9d99d0c83adf47254213866ee7f5c95
Fabio Andrés Sierra Jaraba - Ceiba Software <fabio.sierra@ceiba.com.co>
2020-10-12 10:16:08 -0500

    Se borra anotación de tests

--- src/test/java/com/ceiba/adn/demogjs/DemoApplicationTests.java
+++ src/test/java/com/ceiba/adn/demogjs/DemoApplicationTests.java
@@ -1,12 +1,8 @@
 package com.ceiba.adn.demogjs;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
